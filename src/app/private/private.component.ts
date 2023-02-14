9ee62362081f22e428826a0a83cd3c825d4de0bf
Hackjaku <stressfest@rocketmail.com>
2023-02-14 10:02:51 +0100

    ADD: player/master components

--- src/app/private/private.component.ts
+++ src/app/private/private.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-private',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrivateComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  logout(): void {
+    this.router.navigate(['/public/login']);
   }
 
 }
--- src/app/private/private.module.ts
+++ src/app/private/private.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { PrivateRoutingModule } from './private-routing.module';
 import { PrivateComponent } from './private.component';
+import { MasterComponent } from './master/master.component';
+import { PlayerComponent } from './player/player.component';
 
 
 @NgModule({
   declarations: [
-    PrivateComponent
+    PrivateComponent,
+    MasterComponent,
+    PlayerComponent
   ],
   imports: [
     CommonModule,
