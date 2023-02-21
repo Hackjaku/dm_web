d94a5250a3877bf0bfca36204d46a9e2f4b112f7
Hackjaku <stressfest@rocketmail.com>
2023-02-21 17:09:25 +0100

    UPDATE: player model

--- src/app/interfaces/player.ts
+++ src/app/interfaces/player.ts
@@ -1,4 +1,21 @@
 export interface Player {
   Id: number;
   Name: string;
+  Race: string;
+  Class: string;
+  ArmorClass: number;
+  Str: number;
+  Con: number;
+  Dex: number;
+  Int: number;
+  Wis: number;
+  Cha: number;
+  BaseAttack: number;
+  Fortitude: number;
+  Reflex: number;
+  Will: number;
+  RolledHp: number;
+  Xp: number;
+  Load: number;
+  UserId: number;
 }
