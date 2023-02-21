83d08579c8a6591dedce3fbe0e1c3eb82abb9c23
Hackjaku <stressfest@rocketmail.com>
2023-02-21 17:10:11 +0100

    UPDATEL event model

--- src/app/interfaces/event.ts
+++ src/app/interfaces/event.ts
@@ -1,3 +1,11 @@
 export interface Event {
   Id: number;
+  Name: string;
+  Description: string;
+  Duration: number;
+  EffectId: number;
+  Amount: number;
+  PlayerId: number;
+  ItemId: number;
+  SpellId: number;
 }
