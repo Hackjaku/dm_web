8bcb727ec7f80af889bf4d5a1620b451ab4df9a4
Hackjaku <stressfest@rocketmail.com>
2023-02-22 11:34:49 +0100

    UPDATE: login component

--- src/app/public/login/login.component.ts
+++ src/app/public/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm = this.fb.group({
+    username: [null, Validators.required],
+    password: [null, Validators.required]
+  });
+
+  hidePassword = true;
+
+  loginSub!: Subscription;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthenticationService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestory(): void {
+    this.loginSub.unsubscribe();
+  }
+
+  login(): void {
   }
 
 }
